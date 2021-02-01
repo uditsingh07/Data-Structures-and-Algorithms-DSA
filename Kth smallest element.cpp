cc8d53bc11c773b270064b5d1d3123571f0443ee
Udit singh <udit.singhmkp@gmail.com>
2021-02-02 01:37:52 +0530

    Create Kth smallest element.cpp

---
+++ Kth smallest element.cpp
@@ -0,0 +1,27 @@
+/*Kth smallest element
+
+Given an array arr[] and a number K where K is smaller than size of array, 
+the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Example:
+
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7.
+
+*/
+
+// arr : given array
+// l : starting index of the array i.e 0
+// r : ending index of the array i.e size-1
+// k : find kth smallest element and return using this function
+
+int kthSmallest(int arr[], int l, int r, int k) {
+    sort(arr, arr + (r+1));
+    return arr[k-1];
+}
