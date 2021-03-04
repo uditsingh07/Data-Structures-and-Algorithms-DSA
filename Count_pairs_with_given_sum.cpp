1226f89996a5a52aff2c8f1ab5d1f79bb01bd125
Udit singh <udit.singhmkp@gmail.com>
2021-03-04 18:19:38 +0530

    Update Count_pairs_with_given_sum.cpp

--- Count_pairs_with_given_sum.cpp
+++ Count_pairs_with_given_sum.cpp
@@ -10,7 +10,7 @@ and arr[1] + arr[3] = 5 + 1 = 6.
 */
 
  int getPairsCount(int arr[], int n, int k) {
-   // code here
+   
    int count;
    for (int i = 0; i < n - 1; i++) {
      for (int j = i + 1; j < n; j++) {
