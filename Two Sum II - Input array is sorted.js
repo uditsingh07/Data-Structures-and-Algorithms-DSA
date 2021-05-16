6e4a96958a4e4ece35d61a29b2083d55dfe164bf
Udit singh <udit.singhmkp@gmail.com>
2021-05-16 20:45:22 +0530

    Two Sum II - Input array is sorted

---
+++ Two Sum II - Input array is sorted.js
@@ -0,0 +1,23 @@
+/* Given an array of integers numbers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+*/
+
+var twoSum = function(numbers, target) {
+    let res = [];
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) {
+                res.push(i + 1, j + 1);
+            }
+        }
+    }
+    return res;
+};
