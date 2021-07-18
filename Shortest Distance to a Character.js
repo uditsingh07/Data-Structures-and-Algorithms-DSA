f72fdc2f3c3149c5e1996007daf4b82d8e26d6e0
Udit singh <udit.singhmkp@gmail.com>
2021-07-18 20:02:17 +0530

    Shortest Distance to a Character

---
+++ Shortest Distance to a Character.js
@@ -0,0 +1,29 @@
+/* Given a string s and a character c that occurs in s, return an array of integers answer where answer.length == s.length and 
+answer[i] is the distance from index i to the closest occurrence of character c in s.
+
+The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
+
+Input: s = "loveleetcode", c = "e"
+Output: [3,2,1,0,1,0,0,1,2,2,1,0]
+Explanation: The character 'e' appears at indices 3, 5, 6, and 11 (0-indexed).
+The closest occurrence of 'e' for index 0 is at index 3, so the distance is abs(0 - 3) = 3.
+The closest occurrence of 'e' for index 1 is at index 3, so the distance is abs(1 - 3) = 3.
+For index 4, there is a tie between the 'e' at index 3 and the 'e' at index 5, but the distance is still the same: abs(4 - 3) == abs(4 - 5) = 1.
+The closest occurrence of 'e' for index 8 is at index 6, so the distance is abs(8 - 6) = 2.
+*/
+
+var shortestToChar = function(s, c) {
+    const answer = Array(s.length).fill(Infinity);
+    let l = Infinity, r = Infinity;
+    
+    for(let f = 0; f < s.length; f++) {
+        const b = s.length-1-f;
+        
+        l = s[f] === c ? 0 : l+1;
+        r = s[b] === c ? 0 : r+1;
+        
+        answer[f] = Math.min(answer[f], l);
+        answer[b] = Math.min(answer[b], r);
+    }
+    return answer;  
+};
